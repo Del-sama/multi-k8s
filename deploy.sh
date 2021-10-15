b9de403a814f9bd6500912d415b14853afd0151a
Delores Diei <deloresdiei@gmail.com>
2021-10-15 12:04:40 +0200

    update script

--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 docker build -t delchan/multi-client:latest  -t delchan/multi-client:$SHA -f ./client/Dockerfile ./client
 docker build -t delchan/multi-server:latest  -t delchan/multi-server:$SHA -f ./server/Dockerfile ./server
 docker build -t delchan/multi-worker:latest  -t delchan/multi-worker:$SHA -f ./worker/Dockerfile ./worker
+
 docker push delchan/multi-client:latest
 docker push delchan/multi-client:$SHA
 docker push delchan/multi-server:latest
 docker push delchan/multi-server:$SHA
 docker push delchan/multi-worker:latest
 docker push delchan/multi-worker:$SHA
+
 kubectl apply -f k8s
+
 kubectl set image deployment/server-deployment server=delchan/multi-server:$SHA
-kubectl set image deployment/client-deployment server=delchan/multi-client:$SHA
-kubectl set image deployment/worker-deployment server=delchan/multi-worker:$SHA
+kubectl set image deployment/client-deployment client=delchan/multi-client:$SHA
+kubectl set image deployment/worker-deployment worker=delchan/multi-worker:$SHA
